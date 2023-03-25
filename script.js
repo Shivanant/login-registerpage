2946e01e56543af9638be68fad6837b412e02921
Shivanant <shivanantbhagat7@gmail.com>
2023-03-25 22:08:53 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const wrapper = document.querySelector(".wrapper");
-const registerLink=document.querySelector('.register-link')
-const loginLink=document.querySelector('.login-link')
-const loginbtn=document.querySelector('.btnLogo-popup')
-const iconClose=document.querySelector('.icon-close')
+const registerLink = document.querySelector('.register-link')
+const loginLink = document.querySelector('.login-link')
+const loginbtn = document.querySelector('.btnLogo-popup')
+const loginbtn1 = document.querySelector('.btnLogo-popup1')
+const iconClose = document.querySelector('.icon-close')
+const togglebtn = document.querySelector('.toggle-btn');
+const dropDown= document.querySelector('.drop-down');
 
-registerLink.addEventListener('click',()=>{
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click',()=>{
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
-loginbtn.addEventListener('click',()=>{
+loginbtn.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+loginbtn1.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click',()=>{
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
-    console.log('i am clicked')
-    
+
 });
+togglebtn.addEventListener('click',()=>{
+    console.log("loda lassan");
+    dropDown.classList.toggle('inactive');
+
+})
